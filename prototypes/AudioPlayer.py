8de90f178bd7bbe33eef103a1a9eb9581398d96d
signebtn <82439682+signebtn@users.noreply.github.com>
2021-04-18 13:24:12 +0200

    Update AudioPlayer.py

--- prototypes/AudioPlayer.py
+++ prototypes/AudioPlayer.py
@@ -1,5 +1,57 @@
 # TODO: Signe
-class AudioPlayer():
-
+class AudioPlayer:
+    
+    def __init__(self):
+        pass
+   
     def play(self, file_path):
-        pass 
+         
+        filename = 'output.wav'
+
+        # Set chunk size of 1024 samples per data frame
+        chunk = 1024  
+
+        # Open the sound file 
+        wf = wave.open(filename, 'rb')
+
+        # Create an interface to PortAudio
+        p = pyaudio.PyAudio()
+
+        # Open a .Stream object to write the WAV file to
+        # 'output = True' indicates that the sound will be played rather than recorded
+        stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                        channels = wf.getnchannels(),
+                        rate = wf.getframerate(),
+                        output = True)
+
+        # Read data in chunks
+        data = wf.readframes(chunk)
+
+        # Play the sound by writing the audio data to the stream
+        while data != '':
+            stream.write(data)
+            data = wf.readframes(chunk)
+        
+        # Close and terminate the stream
+        stream.close()
+        p.terminate()
+
+
+player = AudioPlayer()
+        
+t0 = {'source': 'initial', 'target': 'ready'}
+t1 = {'trigger': 'start', 'source': 'ready', 'target': 'playing'}
+t2 = {'trigger': 'done', 'source': 'playing', 'target': 'ready'}
+
+s_playing = {'name': 'playing', 'do': 'play()'}
+
+stm = Machine(name='stm', transitions=[t0, t1, t2], states=[s_playing], obj=player)
+recorder.stm = stm
+
+driver = Driver()
+driver.add_machine(stm)
+driver.start()
+
+print("driver started")
+
+driver.send('start', 'stm')
